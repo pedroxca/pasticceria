5e9ff3738cd23cd86694e8ef26550af8abc502ac
F8236816 Pedro Faustino De Oliveira Rodrigues De Melo <faustinopedro.faustino@gmail.com>
2024-04-01 16:45:50 -0300

    adiciona paginas e formatações

--- src/Pages/Products.tsx
+++ src/Pages/Products.tsx
@@ -1,78 +1,53 @@
-import { CssBaseline } from '@mui/material'
-import { Text } from '../components/Text'
-
+import { Box, Container, Fade, Grid, Grow } from '@mui/material';
+import { Text } from '../components/Text';
+import { Title } from '../components/Title';
 function Products() {
+  const TIMEOUT_IMAGEM = 500
+  const products = [
+    { name: 'Cafe Espresso',     timeout: 1500 + TIMEOUT_IMAGEM  *1 },
+    { name: 'Cappuccino',        timeout: 1700 + TIMEOUT_IMAGEM * 2 },
+    { name: 'Pão de Queijo',     timeout: 1800 + TIMEOUT_IMAGEM * 3 },
+    { name: 'Bolo de Chocolate', timeout: 1900 + TIMEOUT_IMAGEM * 4 },
+    { name: 'Misto Quente',      timeout: 2000 + TIMEOUT_IMAGEM * 5 },
+    { name: 'Salada de Frutas',  timeout: 2100 + TIMEOUT_IMAGEM * 6 },
+    { name: 'Croissant',         timeout: 2200 + TIMEOUT_IMAGEM * 7 },
+  ]
+
   return (
-    <>
-      <CssBaseline enableColorScheme />
-      <div>
-        <Text>
-          Products
-        </Text>
-        <ul>
-          <li>
-            <Text>
-              Café Espresso
-            </Text>
-
-          </li>
-          <li>
-            <Text>
-              Cappuccino Tradicional
-            </Text>
-
-          </li>
-          <li>
-            <Text>
-              Pão de Queijo
-            </Text>
-
-          </li>
-          <li>
-            <Text>
-              Bolo de Chocolate
-            </Text>
-
-          </li>
-          <li>
-            <Text>
-              Sanduíche de Presunto e Queijo
-            </Text>
-
-          </li>
-          <li>
-            <Text>
-              Salada de Frutas
-            </Text>
-
-          </li>
-          <li>
-            <Text>
-              Cookies de Chocolate
-            </Text>
-
-          </li>
-          <li>
-            <Text>
-              Chá Preto
-            </Text>
-
-          </li>
-          <li>
-            <Text>
-              Chocolate Quente
-            </Text>
-
-          </li>
-          <li>
-            <Text>
-              Croissant
-            </Text>
-
-          </li>
-        </ul>
-      </div>
-    </>
+    <Fade in timeout={1500} >
+      <Container maxWidth="lg" sx={{ mt: '1vw' }}>
+      <Title>Nossos Produtos</Title>
+        <Container>
+          <Text sx={{ fontSize: '1.4rem', mb: '1rem', mt: '1rem' }}>
+            Explore os deliciosos produtos da Coffee Shops Tia Rosa. Do nosso café fresco aos nossos bolos caseiros, temos algo para todos os gostos!
+          </Text>
+          <Text sx={{ fontSize: '1.3rem' }}>
+            Alguns de nossos produtos são:
+          </Text>
+          <Box sx={{ flexGrow: 1 }}>
+          </Box>
+          <Grid sx={{ alignItems: 'flex-end', justifyContent: 'center' }} container spacing={1}>
+            {
+              products.map(product => {
+                return (
+                  <Grid key={product.name} item >
+                    <Grow in timeout={product.timeout} style={{ transformOrigin: '0 0 0' }}>
+                      <Container>
+                        <Text>{product.name + '\n'}</Text>
+                        <img
+                          src={`/src/assets/Comidas/${product.name}.jpg`}
+                          style={{ maxHeight: '22vh' }}
+                          loading="lazy" />
+                      </Container>
+                    </Grow>
+                  </Grid>
+                )
+              })
+            }
+          </Grid>
+        </Container>
+      </Container>
+    </Fade >
   )
 }
 
--- src/Pages/WorkWithUs.tsx
+++ src/Pages/WorkWithUs.tsx
@@ -1,11 +1,49 @@
-import { CssBaseline } from '@mui/material'
+import { Container, Fade } from '@mui/material'
+import { Text } from '../components/Text'
+import { Title } from '../components/Title'
 
 function WorkWithUs() {
   return (
-    <>
-      <CssBaseline enableColorScheme />
-      <div>Alteração em HML</div>
-    </>
+    <Fade in timeout={1500} >
+      <Container maxWidth="lg" sx={{ mt: '1vw' }}>
+        <Text>
+          <Title>Junte-se à nossa Equipe!</Title>
+          <br />
+          Você é ágil, proativo e tem vontade de aprender sobre o universo das padarias? Nós, da Coffee Shops Tia Rosa, estamos procurando por você!
+          <br />
+          <br />
+          <strong>
+            Oportunidades de Trabalho:
+          </strong>
+          <br />
+          Atendente de Balcão
+          <br />
+          Auxiliar de Cozinha
+          <br />
+          Barista
+          <br />
+          <br />
+          <strong>
+          Requisitos:
+          </strong>
+          <br />
+          Agilidade e eficiência no atendimento
+          <br />
+          Proatividade e disposição para aprender
+          <br />
+          Disponibilidade para trabalhar em horários flexíveis
+          <br />
+          <br />
+          Envie seu currículo para recrutamento@coffeeshopstiarosa.com para se candidatar a uma vaga.
+          <br />
+          <br />
+          Entre em Contato:
+          Para mais informações, entre em contato pelo telefone (12) 3456-7890.
+          <br />
+          <small>Contratamos sem experiência sujeito a entrevista e treinamento</small>
+        </Text>
+      </Container>
+    </Fade>
   )
 }
 
